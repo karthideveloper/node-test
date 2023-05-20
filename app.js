994e87a768c22a7b330afe1f8f294cbb4777c994
karthi k <karthiideveloper@gmail.com>
2023-05-20 23:04:02 +0530

    temp

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const log=require('./logger')
+const EventEmitter=require('events');
+
+const emitter=new EventEmitter();
+
+emitter.on('messageLogged',(args)=>{
+    console.log("hiiii",args)
+})
+emitter.emit('messageLogged',{id:1,url:'https'})
 
 log('hi');
--- logger.js
+++ logger.js
@@ -1,5 +1,22 @@
+const path=require("path")
+const os=require("node:os")
+const fs=require('node:fs')
+
+
+
 const log=(massage)=>{
 console.log(massage)
+console.log(os.freemem())
+console.log(os.totalmem())
+console.log(path.parse(__filename))
+fs.readdirSync('./')
+fs.readdir('./',(err,res)=>{
+if(!err){
+    console.log(res)
 }
+})
+
+}
+
 
 module.exports=log;
