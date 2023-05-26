39a82ec74805620672e85853bafd761728a10815
karthi k <karthiideveloper@gmail.com>
2023-05-26 22:03:04 +0530

    promises

---
+++ promises.js
@@ -0,0 +1,28 @@
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("hhhh")
+    reject(new Error("text"));
+  }, 2000);
+});
+
+p.then((res) => {
+  console.log("res", res);
+}).catch((err) => {
+  console.log("err", err.message);
+});
+
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("getUserSetTimeout...");
+      resolve({ id: id, name: "karthi" });
+    }, 2000);
+  });
+};
+const r = getUser(2);
+r.then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.log(err);
+});
+console.log(r);
