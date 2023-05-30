39ba7dde8b6c4c73e8fb5336b1cc064ba32c85c1
karthi k <karthiideveloper@gmail.com>
2023-05-30 22:44:19 +0530

    validation

--- mongo.js
+++ mongo.js
@@ -9,35 +9,56 @@ mongoose
   });
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (value, callback) {
+        setTimeout(()=>{
+          const result = value && value.length > 5;
+          callback(result);
+        },4000)
+        
+      },
+      message: "course contain at least one tag ",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+  },
 });
 const Course = mongoose.model("course", courseSchema);
 async function createCourse() {
-  
   const course = new Course({
     name: "Angular js Course",
     author: "Karthi",
-    tags: ["Angular", "frontEnd"],
+    // tags: ["Angular", "frontEnd"],
     isPublished: true,
+    price:12000
   });
   const result = await course.save();
   console.log(result);
 }
 createCourse();
 
-async function getCourse(){
-  const course=await Course
-  .find({author:'karthi'}).limit(10).sort({name:1}).select({name:1,tags:1});
+async function getCourse() {
+  const course = await Course.find({ author: "karthi" })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
   // .find({price:{$gte:10,$lte:20}})
   // .find({price:{$in:[10,20,30]}})
   // .find()
   // .or([{name:"karthi"},{price:"10"}])
   // .and([{name:"karthi"},{price:"10"}])
-  console.log(course)
+  console.log(course);
 }
 
-getCourse();
+// getCourse();
